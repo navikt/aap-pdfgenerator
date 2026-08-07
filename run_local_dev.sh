97e06bfec97bc97b39bbc6a583a63d8ab6bfc83f
Tor Idland <tor.idland@nav.no>
2026-08-07 11:44:43 +0200

    AAP-2194 innsending app i inbound rules i dev

--- run_local_dev.sh
+++ run_local_dev.sh
@@ -10,7 +10,7 @@ docker run \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/data:/app/data \
         -v $CURRENT_PATH/resources:/app/resources \
-        -p 8080:8080 \
+        -p 8089:8080 \
         -e DEV_MODE=true \
         -it \
         --rm \
